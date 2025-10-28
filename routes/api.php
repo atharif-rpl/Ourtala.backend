739171379a6c52127525fea6bdf3e664079f52c1
atharif-rpl <atha55733@gmail.com>
2025-10-28 11:36:47 +0700

    feat: explicitly define POST /donations route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TeamMemberController;
 use App\Http\Controllers\DonationController;
 
+
 Route::get('/test', function () {
     return response()->json([
         'message' => 'Hello from Laravel backend!',
@@ -11,4 +12,5 @@ Route::get('/test', function () {
     ]);
 });
 Route::apiResource('team-members', TeamMemberController::class);
+Route::post('/donations', [DonationController::class, 'store']);
 Route::apiResource('donations', DonationController::class);
