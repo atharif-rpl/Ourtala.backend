6bcc3f069fd5e821c86bf73ea1ea0d0752fc7465
atharif-rpl <atha55733@gmail.com>
2025-10-28 14:29:18 +0700

    Test post

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TeamMemberController;
 use App\Http\Controllers\DonationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 
 Route::get('/test', function () {
@@ -14,3 +16,11 @@ Route::get('/test', function () {
 Route::apiResource('team-members', TeamMemberController::class);
 Route::post('/donations', [DonationController::class, 'store']);
 Route::apiResource('donations', DonationController::class);
+
+Route::post('/test-post', function (Request $request) {
+    Log::info('Test POST route hit successfully.'); // Error akan hilang
+    return response()->json([
+        'message' => 'Test POST route is working!',
+        'data_received' => $request->all()
+    ]);
+});
