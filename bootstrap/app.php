4af0bc4b7b55ecac9599c8f88a1b9044d5f84cb3
atharif-rpl <atha55733@gmail.com>
2025-10-31 14:29:22 +0700

    chore: cleanup CORS config and use Laravel built-in middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,18 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        // apiPrefix: '',<--- untuk deploy di subfolder
-        commands: __DIR__ . '/../routes/console.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(CorsMiddleware::class);
+        // Tidak perlu tambahkan CorsMiddleware custom
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
